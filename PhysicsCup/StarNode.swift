654659cdcd946f7327c91fa2ce765d9ed3bfe4b6
John Fisher <john.fisher@atomicobject.com>
2015-01-14 18:14:43 -0500

    Pushing actual code- whoops.

---
+++ PhysicsCup/StarNode.swift
@@ -0,0 +1,13 @@
+//
+//  StarNode.swift
+//  PhysicsCup
+//
+//  Created by John Fisher on 12/5/14.
+//  Copyright (c) 2014 John Fisher. All rights reserved.
+//
+
+import UIKit
+
+class StarNode: SKSpriteNode {
+   
+}
